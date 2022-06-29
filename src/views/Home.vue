94aa7bedebfdd680ed2b37f70ccef7a6262b1951
qi xu <qi.xu@autronicafire.no>
2022-06-29 14:06:55 +0200

    hide scrollbar globally

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <v-col class="blue-grey--text mt-4" align="center">
+      <h3 class="ma-4">Alarm & Fault Viewer - Version 0.0.2</h3>
+      <h4 class="ma-4">Contact Autromaster Team For Support</h4>
+      <h4 class="ma-4">Autronica Fire and Security AS</h4>
+      <h5 class="ma-4">Year 2022</h5>
+    </v-col>
+  </div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-import vuetify from 'vite-plugin-vuetify'
+import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
-]
-})
+// export default defineConfig({
+//   server: {
+//     port: process.env.SERVE_PORT,
+//   },
+//   plugins: [vue(), vuetify({ autoImport: true })],
+// });
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [vue(), vuetify({ autoImport: true })],
+    server: {
+      port: process.env.VITE_SERVE_PORT,
+    },
+  });
+};
